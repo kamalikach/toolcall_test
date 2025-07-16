920cc7c377b47c2aafd8e936ef8d1e6bd10c3d79
Kamalika Chaudhuri <kamalika@devfair0640.h2.fair>
2025-07-16 14:29:22 +0000

    working example

---
+++ google/tool_definitions.py
@@ -0,0 +1,12 @@
+# tool_definitions.py
+
+EMAIL_FUNCTION_DECLARATION = {
+    "name": "get_email",
+    "description": "Function to return the next email in inbox",
+}
+
+CHAT_FUNCTION_DECLARATION = {
+    "name": "get_chat",
+    "description": "Function to get my next chat message"
+}
+
